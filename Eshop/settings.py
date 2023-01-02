b153a19ea74a265c093c0d044ff666fdfa472706
Shubham Torkade <92077877+shubham062000@users.noreply.github.com>
2023-01-02 17:42:26 +0530

    Update settings.py

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'eshop',
         'USER': 'postgres',
-        'PASSWORD': 'shubham@2002',
+        'PASSWORD': 'root',
         'HOST': 'localhost',
         'PORT': '',
     }
